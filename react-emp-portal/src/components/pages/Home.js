d8cd93ad553be5194a6eea098d6f78aee0f4fb1a
Bhumika Narale <bhumikanarale19@gmail.com>
2022-01-27 10:15:42 +0530

    commit

--- react-emp-portal/src/components/pages/Home.js
+++ react-emp-portal/src/components/pages/Home.js
@@ -3,6 +3,7 @@ import { deepPurple, green } from '@material-ui/core/colors';
 import List from "../student/List";
 import axios from "axios";
 import { useState } from "react";
+
 const useStyles = makeStyles({
  headingColor: {
   backgroundColor: deepPurple[400],
@@ -43,26 +44,30 @@ const Home = () => {
  }
  return (
   <div className="container">
-   <Box textAlign="center" className={classes.headingColor} p={2} mb={2}>
-    <Typography variant="h2">Employee Data</Typography>
-   </Box>
-   <Grid container justify="center" spacing={4}>
-    <Grid item md={6} xs={12}>
-     <Box textAlign="center" p={2} className={classes.addStuColor} mb={2}>
-      <Typography variant="h4">Add Employee</Typography>
-     </Box>
-     <form noValidate>
-      <Grid container spacing={2}>
-       <Grid item xs={12}>
-        <TextField autoComplete="stuname" name="stuname" variant="outlined" required fullWidth id="stuname" label="Name" onChange={e => onTextFieldChange(e)}
-        />
-       </Grid>
-       <Grid item xs={12}>
-        <TextField autoComplete="email" name="email" variant="outlined" required fullWidth id="email" label="Email Address" onChange={e => onTextFieldChange(e)} />
-       </Grid>
+    <Box textAlign="center" className={classes.headingColor} p={2} mb={2}>
+        <Typography variant="h2">Employee Data</Typography>
+    </Box>
+    <Grid container justify="center" spacing={4}>
+        <Grid item md={6} xs={12}>
+            <Box textAlign="center" p={2} className={classes.addStuColor} mb={2}>
+                <Typography variant="h4">Add Employee</Typography>
+            </Box>
+
+        <form noValidate>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField autoComplete="stuname" name="stuname" variant="outlined" 
+                required fullWidth id="stuname" label="Name" onChange={e => onTextFieldChange(e)}/>
+            </Grid>
+
+            <Grid item xs={12}>
+                <TextField autoComplete="email" name="email" variant="outlined" 
+                required fullWidth id="email" label="Email Address" onChange={e => onTextFieldChange(e)} />
+            </Grid>
       </Grid>
       <Box m={3}>
-       <Button type="submit" variant="contained" color="primary" fullWidth onClick={e => onFormSubmit(e)}>Add</Button>
+       <Button type="submit" variant="contained" color="primary" 
+       fullWidth onClick={e => onFormSubmit(e)}>Add</Button>
       </Box>
      </form>
     </Grid>
